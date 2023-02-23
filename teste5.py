2f53762d29d49ccf9c72568f1ec9c09aac5e7c4f
Moisés A. C. tulitekule <moisestulitekule7@gmail.com>
2023-02-23 15:28:23 +0100

    my python version 2

--- teste5.py
+++ teste5.py
@@ -12,11 +12,26 @@
 
 # print('acabou')   
 
-start = 0
-end = 10
+# start = 0
+# end = 10
 
-while start < end:
-    start += 1
-    print(start)
+# while start < end:
+#     start += 1
+#     print(start)()
+
+qtd_linha = 5
+qtd_coluna = 5
+
+linha = 1
+while linha <= qtd_linha:
+    coluna=1
+    while coluna <= qtd_coluna:
+        print(f'{linha=} {coluna=}')
+        coluna +=1
+    
+    linha +=1
+    
+
+print('acabou')    
 
    
