8e5eb4ba252283736eaec1a03e01db4ba51eaf3d
Moisés A. C. tulitekule <moisestulitekule7@gmail.com>
2023-02-23 15:58:24 +0100

    my python version 3

--- aula18.py
+++ aula18.py
@@ -5,6 +5,8 @@ while True:
     operador  = input('Digite um operador(+-/*): ')
 
     numross_validos = None
+    num1_float = 0
+    num2_float = 0
 
     try:
         num1_float = float(number1)
@@ -12,6 +14,31 @@ while True:
         numross_validos = True
     except :
         numross_validos = None
+
+    if numross_validos is None:
+        print('um ou ambos os numeros digitados sao invalidos.')
+        continue
+
+    operador_permitidos = '+-/*'
+
+    if operador not in operador_permitidos:
+        print('operador invalido')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador')
+
+    print('Realizando sua conta, confira o valor a baixo')
+    if operador == '+':
+        print(num1_float + num2_float)
+    elif operador == '-':
+         print(num1_float - num2_float)
+    elif operador == '/':
+         print(num1_float / num2_float)
+    elif operador == '*':
+         print(num1_float * num2_float) 
+    else:
+        print('nunca teria chegado aqui')                  
 
     sair = input(' Quer sair? [s]im').lower().startswith('s')
 
